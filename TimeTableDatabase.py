7370b39267d2f34d6936f911c74a88ee92816644
Sina Kokab <sinakokab@gmail.com>
2018-11-21 17:29:55 +0000

    Update TimeTableDatabase.py

--- TimeTableDatabase.py
+++ TimeTableDatabase.py
@@ -1,193 +1,213 @@
-import requests, bs4, time, sqlite3, TimeTableDatabase, threading
-import sys, os, datetime
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+import sqlite3, sys, os
+from sqlite3 import Error
 
-TimeTable_Url = "https://timetable.dundee.ac.uk/Scientia/SWS/Dundee1819/default.aspx"
-TimeTable_Module_Select_Button = "LinkBtn_modules"
-TimeTable_Department_ID = "dlFilter"
-TimeTable_School_Value = "B8FB2C52859E2190D0FDBC07A892F3D4"
-TimeTable_Module_Select_Id = "dlObject"
-TimeTable_Module_Paraclinical = "DS20006-SEM1-2-A"
-TimeTable_Module_ClinicalIntroduction = "DS22007-SEM1-2-A"
-TimeTable_ViewTimeTable = "bGetTimetable"
-TimeTable_Combine_Button = '//*[@id="RadioType_2"]'
+def OpenDatabase():
+    try:
+        Timetable_Database = sqlite3.connect('TimeTableDatabase.sqlite')
+        cursor = Timetable_Database.cursor()
+        print("Database Opened Successfully")
 
-def scrapetimer():
-    threading.Timer(3600.0, scrapetimer).start()
-    print("Scrape timer activating")
-    scrapeupdatetodb()
+        try:
+            cursor.execute('''CREATE TABLE IF NOT EXISTS Lectures(
+             LectureID TEXT NOT NULL,
+             LectureType TEXT NOT NULL,
+             LectureStartTime TEXT NOT NULL,
+             LectureEndTime TEXT NOT NULL,
+             LectureDuration TEXT NOT NULL,
+             LectureWeek TEXT NOT NULL,
+             Lecturer TEXT NOT NULL,
+             LectureLocation TEXT NOT NULL,
+             LectureDay TEXT NOT NULL
+             );''')
+
+            print("Created database table")
+            Timetable_Database.close()
+        except Error as e:
+            print("Database Error")
+            ExceptionInfo()
+            print(e)
+
+        Timetable_Database.close()
+
+    except Error as e:
+        print("Database Error")
+        ExceptionInfo()
+        print(e)
 
 
+def ClearDB():
+        try:
+            Timetable_Database = sqlite3.connect('TimeTableDatabase.sqlite')
+            cursor = Timetable_Database.cursor()
+            print("Database Opened Successfully")
+
+            try:
+                TimeTableContentCheck = "SELECT * FROM Lectures"
+                cursor.execute(TimeTableContentCheck)
+                Check = cursor.fetchone() # Checks if lecture timetable is empty, if not empties it out (To prevent repeated results)
+
+                if len(Check) > 0:
+                    print("Deleting table")
+                    cursor.execute("DROP TABLE Lectures")
+            except:
+                print("Lecture table doesn't exist yet")
+
+            try:
+                cursor.execute('''CREATE TABLE IF NOT EXISTS Lectures(
+                 LectureID TEXT NOT NULL,
+                 LectureType TEXT NOT NULL,
+                 LectureStartTime TEXT NOT NULL,
+                 LectureEndTime TEXT NOT NULL,
+                 LectureDuration TEXT NOT NULL,
+                 LectureWeek TEXT NOT NULL,
+                 Lecturer TEXT NOT NULL,
+                 LectureLocation TEXT NOT NULL,
+                 LectureDay TEXT NOT NULL
+                 );''')
+
+                print("Created database table")
+                Timetable_Database.close()
+            except Error as e:
+                print("Database Error")
+                ExceptionInfo()
+                print(e)
+        except:
+            pass
 
 
-
-def StripHTML(HTMLSTRIPLIST, Text):
-    print(Text)
-    print(HTMLSTRIPLIST)
-    print("STRIPPING HTML")
-    for HTML in HTMLSTRIPLIST:
-        print("HTML TERM " + str(HTML))
-        if HTML in Text:
-            print("HTML FOUND")
-            for term in HTMLSTRIPLIST:
-                Text = Text.replace(HTML, "")
-
-    return Text
 
 def ExceptionInfo():
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print(exc_type, fname, exc_tb.tb_lineno)
 
-def CommitToDb(listofvalues, classes):
-    TimeTableDatabase.ClearDB()
-    HTMLTOSTRIP2 = ['<td>', '</td>', '<tr>', '</tr>', '<tr class="columnTitles">', 'Activity', 'Type', 'Start', 'End', 'Duration', 'Weeks', 'Staff', 'Room']
-    i = 0
-    x = 0
+def CommitToDatabase(values, columns):
+    if columns == "all":
+        columns = ["LectureID", "LectureType", "LectureStartTime", "LectureEndTime", "LectureDuration", "LectureWeek", "Lecturer", "LectureLocation", "LectureDay"]
 
-    while i < len(listofvalues):
-
-        if x == (len(classes[i]) + 1):
-            continue
-
-        while x < len(classes[i]):
-
-            DBInput = StripHTML(HTMLTOSTRIP2, str(classes[i][x]))
-
-            if DBInput != "":
-                print("inputting DB")
-                DBInputList = DBInput.splitlines()
-            if i == 0:
-                DBInputList.append("Monday")
-            elif i == 1:
-                DBInputList.append("Tuesday")
-            elif i == 2:
-                DBInputList.append("Wednesday")
-            elif i == 3:
-                DBInputList.append("Thursday")
-            elif i == 4:
-                DBInputList.append("Friday")
-            for line in DBInputList:
-                if line == "":
-                    DBInputList.remove(line)
-
-
-
-            print("DB LIST:")
-            print(DBInputList)
-            print(len(DBInputList))
-            TimeTableDatabase.CommitToDatabase(DBInputList, "all")
-            # check if a row with the lecture ID exists, and if it does, instead update the row instead of making a copy
-
-            x += 1
-
-        i += 1
-        x = 0
-
-def scrapeupdatetodb():
-
-    global TimeTable_Url
-    global TimeTable_Module_Select_Button
-    global TimeTable_Department_ID
-    global TimeTable_School_Value
-    global TimeTable_Module_Select_Id
-    global TimeTable_Module_Paraclinical
-    global TimeTable_Module_ClinicalIntroduction
-    global TimeTable_ViewTimeTable
-    global TimeTable_Combine_Button
-
-    listofvalues = []
+    if len(values) != len(columns):
+        print("Not enough values in list provided to be committed to database")
+        return None
 
     try:
-        #browser = webdriver.Chrome()
-        #TEST#
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
-        browser = webdriver.Chrome(driver_path='/home/dev/chromedriver', chrome_options=chrome_options,
-        service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
-        #/TEST#
+        Timetable_Database = sqlite3.connect('TimeTableDatabase.sqlite')
+        cursor = Timetable_Database.cursor()
+        #ClearDB()
 
-        browser.get(TimeTable_Url)
-
-        try:
-            elementfound = -1
-            ModuleID_Selector = browser.find_element_by_id(TimeTable_Module_Select_Button)
-            elementfound = 0
-            ModuleID_Selector.click()
-            time.sleep(1)
-            DepartmentID_Selector = browser.find_element_by_id(TimeTable_Department_ID)
-            elementfound = 1
-            SchoolID_Selector = browser.find_element_by_xpath(('//option[@value="%s"]') % (TimeTable_School_Value))
-            elementfound = 2
-            print("1: Found both elements")
-        except:
-            print("1: Didn't find element - " + str(elementfound))
-
-        DepartmentID_Selector.click()
-        time.sleep(1)
-        SchoolID_Selector.click()
-
-        try:
-            elementfound = 0
-            ParaClinical_Module_Select = browser.find_element_by_xpath(('//option[@value="%s"]') % (TimeTable_Module_Paraclinical))
-            elementfound = 1
-            Clinical_Module_Select = browser.find_element_by_xpath(('//option[@value="%s"]') % (TimeTable_Module_ClinicalIntroduction))
-            elementfound = 2
-            ViewTimeTable_Button_Select = browser.find_element_by_id(TimeTable_ViewTimeTable)
-            elementfound = 3
-            TimeTable_Combine_Button_Select = browser.find_element_by_xpath(TimeTable_Combine_Button)
-            print("2: Found all 4 elements")
-        except:
-            print("2: Didn't find element - " + str(elementfound))
-            # Add fail safes to scraper, so that if no data is scraped DB isn't wrecked
-
-        TimeTable_Combine_Button_Select.click()
-
-        ActionChains(browser) \
-            .key_down(Keys.CONTROL) \
-            .click(ParaClinical_Module_Select) \
-            .click(Clinical_Module_Select)\
-            .key_up(Keys.CONTROL) \
-            .perform()
-
-        time.sleep(1)
-
-        ViewTimeTable_Button_Select.click()
-
-        time.sleep(3)
-
-        soup = bs4.BeautifulSoup(browser.page_source, 'html.parser')
-        listofvalues = soup.find_all("table", "spreadsheet")
-
-        i = 0
-        classes = [] # Add fail safes to scraper, so that if no data is scraped DB isn't wrecked
-
-        while i < len(listofvalues):#list_of_inner_text):
-            pprint.pprint(listofvalues[i])#(list_of_inner_text[i])
-
-            if listofvalues[i] == "":
-                print("No data in scrape; ERROR")
-                break
-
-            holdover = listofvalues[i] #all the lectures by the day
-
-            soup2 = bs4.BeautifulSoup(str(holdover), 'html.parser')
-
-            classes.append(i) #all the lectures divided up
-            classes[i] = (soup2.find_all("tr"))
-            print("\n")
-            i += 1
-
-        browser.quit()
-
-        CommitToDb(listofvalues, classes)
-    except:
-        print("Update DB error")
+    except Error as e:
+        print("Database Error")
         ExceptionInfo()
-        pass
+        print(e)
 
-scrapeupdatetodb()
-scrapetimer()
+
+    try:
+        sql = 'insert into Lectures({}) values({})'.format(
+                ', '.join(columns),
+                ', '.join('?' for _ in columns),
+                )
+        cursor.execute(sql, values)
+        Timetable_Database.commit()
+        Timetable_Database.close()
+
+
+        print("Committed values to Database")
+    except Error as e:
+        print("Database Error...")
+        ExceptionInfo()
+        print(e)
+
+
+        #failure to commit to database
+def GetSpecificClassType(type, day):
+        try:
+            Timetable_Database = sqlite3.connect('TimeTableDatabase.sqlite')
+            cursor = Timetable_Database.cursor()
+            print("Database Opened Successfully")
+
+        except Error as e:
+
+            print("Database error...")
+            ExceptionInfo()
+            print(e)
+
+        try:
+
+            print("Fetching Lectures on " + str(day))
+            cursor.execute("SELECT * FROM Lectures WHERE LectureType='{columnchoice}' AND LectureDay='{daychoice}'".\
+            format(columnchoice = type, daychoice = day)) #(day,).\
+            #format(columnchoice = type))
+            all_rows = cursor.fetchall()
+            print(all_rows)
+            Timetable_Database.close()
+            return(all_rows)
+
+        except Error as e:
+
+            print("Database error...")
+            ExceptionInfo()
+            print(e)
+            Timetable_Database.close()
+            return ""
+
+        Timetable_Database.close()
+
+
+def GetLecturesOnDay(day):
+    #Query DB for lectures w/ result of {day} on LectureDay column
+    #Rank them by starting date
+
+    try:
+        Timetable_Database = sqlite3.connect('TimeTableDatabase.sqlite')
+        cursor = Timetable_Database.cursor()
+        print("Database Opened Successfully")
+
+    except Error as e:
+
+        print("Database error...")
+        ExceptionInfo()
+        print(e)
+
+    try:
+
+        print("Fetching Lectures on " + str(day))
+        cursor.execute('SELECT * FROM Lectures WHERE LectureDay= ?', (day,)) #{daychosen}#.\
+            #format(daychosen = day))
+        all_rows = cursor.fetchall()
+        print(all_rows)
+        Timetable_Database.close()
+        return(all_rows)
+
+    except Error as e:
+
+        print("Database error...")
+        ExceptionInfo()
+        print(e)
+        Timetable_Database.close()
+        return ""
+
+    #Timetable_Database.commit()
+    Timetable_Database.close()
+
+def parse_results(listofvalues):
+    #Parses results from GetLecturesOnDay
+    #Returns only simple values, i.e. Lecture Name/Type/ Start & End + Place
+    ListOfValuesToReturn = []
+
+    valuestoinclude = [0, 1, 2, 3, 7] #Index values according to DB structure
+    try:
+        for i in range(0, len(listofvalues)):
+            ListOfValuesToReturn.append([])
+            for x in range(0, len(list(listofvalues[i]))): #range(0, len(listofvalues[i])):
+                if x in valuestoinclude:
+                    ListOfValuesToReturn[i].append(str(listofvalues[i][x]))
+    except:
+        print("Parsing results error")
+        ExceptionInfo()
+
+    #print(ListOfValuesToReturn)
+    return ListOfValuesToReturn
+
+
+
+OpenDatabase()
+#GetLecturesOnDay("Monday")
