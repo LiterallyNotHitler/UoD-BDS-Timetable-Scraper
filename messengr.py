657e62b98920ab3421f4ca8c5a6c2dd076259b91
Sina Kokab <sinakokab@gmail.com>
2018-11-22 00:55:20 +0000

    Update messengr.py

--- messengr.py
+++ messengr.py
@@ -53,7 +53,7 @@ def get_message(sender_id, entity, value):
     print("Deciphering intent...")
 
     if len(entity) > 1: #Checks if there is a class/lecture intent and date time intent
-        pass
+        print("Passing...")
     else:
         send_message(sender_id, "Please retype the message more clearly.")
         return None
