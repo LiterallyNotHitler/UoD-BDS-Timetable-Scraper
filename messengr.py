e271276fb43d095a03e53134fed90ede77000048
Sina Kokab <sinakokab@gmail.com>
2018-11-22 20:52:23 +0000

    Update messengr.py

--- messengr.py
+++ messengr.py
@@ -98,12 +98,12 @@ def ChooseMessage(sender_id, entity, value, classtypestring):
     print("Getting correct user response...")
     try:
         #print(value[entity.index(str("u'microbiology labs'"))])
-        print(value[entity.index('lab_check')])
-        print("Getting day of request... (%s)" % str(value[(value.index("u'datetime"))].strftime('%A')))
+        #print(value[entity.index('lab_check')])
+        print("Getting day of request... (%s)" % str(value[(value.index('datetime'))].strftime('%A')))
         if 'class_check' in entity or 'lecture_check' in entity:
-            results = TimeTableDatabase.GetLecturesOnDay(value[1].strftime('%A'))
+            results = TimeTableDatabase.GetLecturesOnDay(value[(value.index('datetime'))].strftime('%A'))
         elif 'lab_check' in entity or 'clinic_check' in entity:
-            results = TimeTableDatabase.GetSpecificClassType("Lab_Practical", value[(value.index("u'datetime"))].strftime('%A'))
+            results = TimeTableDatabase.GetSpecificClassType("Lab_Practical", value[(value.index('datetime'))].strftime('%A'))
             print(type(entity))
 
         if len(results) != 0:
@@ -130,7 +130,7 @@ def ChooseMessage(sender_id, entity, value, classtypestring):
             response.replace("]", "")
             send_message(sender_id, response)#str(parsedresults[i]) + "\n\n")
     else:
-        send_message(sender_id, "No classes on %s" % str(value[1].strftime('%A')))
+        send_message(sender_id, "No classes on %s" % str(value[(value.index('datetime'))].strftime('%A')))
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
