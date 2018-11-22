ea7d0e516e87a2175e1db42deec786dbe9185c22
Sina Kokab <sinakokab@gmail.com>
2018-11-22 18:02:56 +0000

    Update messengr.py

--- messengr.py
+++ messengr.py
@@ -97,9 +97,9 @@ def ChooseMessage(sender_id, entity, value, classtypestring):
     print("Getting correct user response...")
     try:
         print("Getting day of request... (%s)" % str(value[1].strftime('%A')))
-        if "class_check" in entity or "lecture_check" in entity:
+        if 'class_check' in entity or 'lecture_check' in entity:
             results = TimeTableDatabase.GetLecturesOnDay(value[1].strftime('%A'))
-        elif "lab_check" in entity or "clinic_check" in entity:
+        elif 'lab_check' in entity or 'clinic_check' in entity:
             results = TimeTableDatabase.GetSpecificClassType("Lab_Practical", value[(value.index("u'datetime"))].strftime('%A'))
             print(type(entity))
 
