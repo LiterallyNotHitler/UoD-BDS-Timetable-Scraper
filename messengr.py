2875a2e0fd8cebd3ee82078179ddcb2f05122855
Sina Kokab <sinakokab@gmail.com>
2018-11-22 00:49:22 +0000

    Update messengr.py

--- messengr.py
+++ messengr.py
@@ -128,5 +128,5 @@ def send_message(recipient_id, response):
     return "success"
 
 if __name__ == "__main__":
-    timetablescraper.scrapetimer()
+    #timetablescraper.scrapetimer()
     app.run()
