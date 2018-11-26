f74154e3bd858dcddf3439f35a90cc86d097831c
Sina Kokab <sinakokab@gmail.com>
2018-11-26 00:38:54 +0000

    Update messengr.py

--- messengr.py
+++ messengr.py
@@ -82,7 +82,8 @@ def get_message(sender_id, entity, value):
 
     try:
         print(datetime.date.today().strftime('%V'))
-        if value[(entity.index('datetime'))].strftime('%V') != datetime.date.today().strftime('%V'):
+        print(value[(entity.index('datetime'))].strftime('%V'))
+        if value[(entity.index('datetime'))].strftime('%V') == datetime.date.today().strftime('%V'):
             print("Request is current date...") #Checks if request is checking for current week
         else:
             send_message(sender_id, "Only this week's lectures can be shown.")
